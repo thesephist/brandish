65ac12f44c37ddf5c2f63990dfa6fff25d274b29
Linus Lee <linus@thesephist.com>
2019-10-09 04:44:33 -0700

    Closer to MVP

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+app.use(bodyParser.json({
+    extended: false,
+}));
+
+const CONFIG = require('../config.js');
+
+function respondWithIndex(req, res) {
+    fs.readFile('static/index.html', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        res.set('Content-Type', 'text/html');
+        res.send(data);
+    });
+}
+
+app.get('/', respondWithIndex);
+app.get('/:guideKey/colors/:base/:secondary/:tertiary', respondWithIndex);
+
+app.use('/static', express.static('static'));
+
+app.listen(
+    CONFIG.PORT,
+    () => console.log(`Brandish running on :${CONFIG.PORT}`)
+);
 
